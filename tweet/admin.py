e66b471fa9fdb815629ed3a45e610a84d64eeda3
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-11 01:14:13 +0500

    navbar and model,forms

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+ 
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('user','text','photo','created_at','updated_at')
+
+admin.site.register(Tweet, TweetAdmin)
